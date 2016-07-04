a47c8db05d76427adfc689c80826c58dec4db146
yzbbanban <yzbbanban@live.com>
2016-07-04 11:23:54 +0800

    自定义画线

--- src/com/tarena/customerview/CustomView.java
+++ src/com/tarena/customerview/CustomView.java
@@ -9,45 +9,106 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 public class CustomView extends View {
-
 	int viewHeight;
 	int viewWidth;
 	ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
 	int gap;
+	int maxPrice;
+	int touchY;
+
 	public CustomView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 
 	}
 
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// y坐标位置
+		touchY = (int) event.getY();
+		// x坐标位置
+		int x = (int) event.getX();
+		int position = x / gap;
+		Toast.makeText(getContext(), "position=" + position, Toast.LENGTH_SHORT)
+				.show();
+		return super.onTouchEvent(event);
+	}
+
 	public void setData(ArrayList<HashMap<String, String>> list) {
 		this.list = list;
+		for (int i = 0; i < list.size(); i++) {
+			HashMap<String, String> map = list.get(i);
+			int price = Integer.parseInt(map.get("price"));
+			if (price > maxPrice) {
+				maxPrice = price;
+			}
+		}
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
-		Paint paint = new Paint();
-		paint.setColor(Color.BLACK);
-		Rect rect = new Rect(0, 0, viewWidth, viewHeight);
-		// canvas画矩形，文字，图
-		canvas.drawRect(rect, paint);
-		
-		paint.setColor(Color.WHITE);
-		paint.setTextSize(120);
-		gap=(viewWidth-120)/(list.size()-1);
-		for (int i = 0; i < list.size(); i++) {
-			HashMap<String, String> map=list.get(i);
-			
-			String time=map.get("time");
-			canvas.drawText(time, i*gap, viewHeight, paint);
-			
+		try {
+			Paint paint = new Paint();
+
+			paint.setColor(Color.BLACK);
+			Rect rect = new Rect(0, 0, viewWidth, viewHeight);
+			// canvas画矩形，文字，图
+			canvas.drawRect(rect, paint);
+
+			paint.setColor(Color.RED);
+			if (touchY >= 1) {
+				canvas.drawLine(0, touchY, viewWidth, touchY, paint);
+			}
+
+			paint.setColor(Color.WHITE);
+			paint.setTextSize(50);
+			// 得到最后一个时间的宽度
+			String strLastTime = list.get(list.size() - 1).get("time");
+			// 用矩形得到宽度
+			Rect sizeRect = new Rect();
+			paint.getTextBounds(strLastTime, 0, strLastTime.length(), sizeRect);
+			int lastTimeWidth = sizeRect.width();
+			gap = (viewWidth - 50) / (list.size() - 1);
+			for (int i = 0; i < list.size(); i++) {
+				HashMap<String, String> map = list.get(i);
+				int timeX = i * gap;
+				String time = map.get("time");
+				canvas.drawText(time, timeX, viewHeight, paint);
+				// 畫價格
+				String strPrice = map.get("price");
+				int intPrice = Integer.parseInt(strPrice);
+				int priceHeight = viewHeight - sizeRect.height();
+				int priceY = intPrice * priceHeight / maxPrice;
+
+				// 最大值，显示在下面，应该显示在上面
+				priceY = priceHeight - priceY;
+				// 最大值看不到，畫到view的最上面，y要叫上畫的高度
+				priceY = priceY + sizeRect.height();
+				canvas.drawText(strPrice, timeX, priceY, paint);
+				// 画线
+				if (i < list.size() - 1) {
+					float startX = timeX;
+					float startY = priceY;
+					float stopX = (i + 1) * gap;
+					float stopY = 0;
+					int nextPrice = Integer.parseInt(list.get(i + 1).get(
+							"price"));
+					stopY = nextPrice * priceHeight / maxPrice;
+					stopY = priceHeight - stopY;
+					stopY = stopY + sizeRect.height();
+					canvas.drawLine(startX, startY, stopX, stopY, paint);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
-		
-	}	
-	
+
+	}
+
 	@Override
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 		super.onSizeChanged(w, h, oldw, oldh);
--- src/com/tarena/customerview/MainActivity.java
+++ src/com/tarena/customerview/MainActivity.java
@@ -2,14 +2,18 @@ package com.tarena.customerview;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 	CustomView customView;
+	Random random = new Random();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,27 +21,40 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		customView = (CustomView) findViewById(R.id.cv);
 		customView.setData(getData());
+		final Handler handler = new Handler();
+		handler.postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				customView.setData(getData());
+				Log.i("更新", "获取时间" + System.currentTimeMillis());
+
+				// invalidate 让屏幕无效-->重新绘制
+				customView.invalidate();
+				handler.postDelayed(this, 1000);
+			}
+		}, 1000);
 	}
 
 	public ArrayList<HashMap<String, String>> getData() {
 		HashMap<String, String> map = new HashMap<String, String>();
 		map.put("time", "9");
-		map.put("price", "1000");
+		map.put("price", String.valueOf(random.nextInt(5000)));
 		ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
 		list.add(map);
 		map = new HashMap<String, String>();
 		map.put("time", "10");
-		map.put("price", "2500");
+		map.put("price", String.valueOf(random.nextInt(5000)));
 		list.add(map);
 
 		map = new HashMap<String, String>();
 		map.put("time", "11");
-		map.put("price", "3500");
+		map.put("price", String.valueOf(random.nextInt(5000)));
 		list.add(map);
 
 		map = new HashMap<String, String>();
 		map.put("time", "13");
-		map.put("price", "5000");
+		map.put("price", String.valueOf(random.nextInt(5000)));
 		list.add(map);
 		Log.i("", "");
 		return list;
